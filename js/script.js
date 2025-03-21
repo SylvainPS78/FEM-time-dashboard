684877788ad95307fe1c956b7c047260b8edc751
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-21 10:29:03 +0700

    FEAT Add Buttons fonction with JS

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const dailyBtn = document.getElementById("daily-btn");
+const weeklyBtn = document.getElementById("weekly-btn");
+const monthlyBtn = document.getElementById("monthly-btn");
+const dailyValues = document.querySelectorAll(".daily");
+const weeklyValues = document.querySelectorAll(".weekly");
+const monthlyValues = document.querySelectorAll(".monthly");
+
+dailyBtn.addEventListener("click", () => {
+    dailyValues.forEach(element => {
+        element.style.display = "block";
+    })
+    weeklyValues.forEach(element => {
+        element.style.display = "none";
+    })
+    monthlyValues.forEach(element => {
+        element.style.display = "none";
+    })
+})
+
+
+weeklyBtn.addEventListener("click", () => {
+    dailyValues.forEach(element => {
+        element.style.display = "none";
+    })
+    weeklyValues.forEach(element => {
+        element.style.display = "block";
+    })
+    monthlyValues.forEach(element => {
+        element.style.display = "none";
+    })
+})
+
+monthlyBtn.addEventListener("click", () => {
+    dailyValues.forEach(element => {
+        element.style.display = "none";
+    })
+    weeklyValues.forEach(element => {
+        element.style.display = "none";
+    })
+    monthlyValues.forEach(element => {
+        element.style.display = "block";
+    })
+})

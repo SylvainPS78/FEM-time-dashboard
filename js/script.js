f04ff466ce69506ec4563bc08bfef076627863d6
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-21 11:16:41 +0700

    FEAT Add fetch data from data.json

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const monthlyBtn = document.getElementById("monthly-btn");
 const dailyValues = document.querySelectorAll(".daily");
 const weeklyValues = document.querySelectorAll(".weekly");
 const monthlyValues = document.querySelectorAll(".monthly");
+const cardTimeDaily = document.querySelectorAll(".card-time.daily");
+const cardTimeWeekly = document.querySelectorAll(".card-time.weekly");
+const cardTimeMonthly = document.querySelectorAll(".card-time.monthly");
+const previousTimeDaily = document.querySelectorAll(".previous-time.daily");
+const previousTimeWeekly = document.querySelectorAll(".previous-time.weekly");
+const previousTimeMonthly = document.querySelectorAll(".previous-time.monthly");
 
 dailyBtn.addEventListener("click", () => {
     dailyValues.forEach(element => {
@@ -39,4 +45,22 @@ monthlyBtn.addEventListener("click", () => {
     monthlyValues.forEach(element => {
         element.style.display = "block";
     })
+})
+
+const populateTimes = (data) => {
+    let i = 0;
+    data.forEach(element => {
+        cardTimeDaily[i].textContent = `${element.timeframes.daily.current}hrs`;
+        previousTimeDaily[i].textContent = `Last Day - ${element.timeframes.daily.previous}hrs`;
+        cardTimeWeekly[i].textContent = `${element.timeframes.weekly.current}hrs`;
+        previousTimeWeekly[i].textContent = `Last Week - ${element.timeframes.weekly.previous}hrs`;
+        cardTimeMonthly[i].textContent = `${element.timeframes.monthly.current}hrs`;
+        previousTimeMonthly[i].textContent = `Last Month - ${element.timeframes.monthly.previous}hrs`;
+        i++;
+    })
+}
+
+fetch('/data.json').then((response) => {
+    if(!response.ok) return console.log("Oops ! Couldn't fetch data.json");
+    return response.json().then((data) => {populateTimes(data)});
 })
